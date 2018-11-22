8a1186f1b48248c3ee14c19ca3748d2d30433ba3
ophiraz <45260125+ophiraz@users.noreply.github.com>
2018-11-22 09:42:53 -1000

    Update cameraClient.py

--- cameraClient.py
+++ cameraClient.py
@@ -1,24 +1,32 @@
 import socket
-import getImage
+from saveImage import save_image
+from time import sleep
 
-SERVER_IP = "192.168.43.24" #"192.168.200.76"
-SERVER_PORT = 1234
+SERVER_IP = "192.168.43.24"
+SERVER_PORT = 4321
+PATH = "/home/pi/Documents/moona/image.jpg"
 
-def getImege(path):
-    #get imege
-    img = open(path, "r")
+def getImage(path):
+    #save image from the camera
+    save_image(path)
     
-    print(img.read())
-    return img.read()
+    #get imege
+    imageFile= open(path, "rb")
+    
+    return imageFile
 
-def sendData(msg):
+def sendData(imageFile):
+    
     # Create a TCP/IP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
     try:
         # Connecting to remote computer 1234
         sock.connect((SERVER_IP, SERVER_PORT))
-        # Sending data to server
-        sock.sendall(msg.encode())
+        l = imageFile.read()
+        # Sending data to server 
+        sock.sendall(l)
+        
     except socket.error as errorMsg:
         print("ERROR!!! meage not sent. server is probiboly down\n")
         # Closing the socket
@@ -28,11 +36,14 @@ def sendData(msg):
     sock.close()
 
 def main():
-    msg2 = "Gboy\n"
-    
-    #msg = getImege()
-    sendData(msg2)
+    #msg2 = "Gboy\n"
+    while (True):        
+        #get the data
+        imageFile = getImage(PATH)
+        
+        #send the msg to the server
+        sendData(imageFile)
 
+       
 if __name__ == "__main__":
     main()
-
